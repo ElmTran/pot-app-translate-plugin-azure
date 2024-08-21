33a4f871ce498424f50d3d8278b6dddd7332ae55
ElmTran <c897611977@gmail.com>
2024-08-21 18:15:37 +0800

    v0.0.1

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 async function translate(text, from, to, options) {
-    const { config, utils } = options;
-    const { tauriFetch: fetch } = utils;
-    let { requestPath: url } = config;
-    let plain_text = text.replaceAll("/", "@@");
-    let encode_text = encodeURIComponent(plain_text);
-    if (url === undefined || url.length === 0) {
-        url = "lingva.pot-app.com"
-    }
-    if (!url.startsWith("http")) {
-        url = `https://${url}`;
-    }
-    const res = await fetch(`${url}/api/v1/${from}/${to}/${encode_text}`, {
-        method: 'GET',
-    });
+  const { config, utils } = options;
+  const { tauriFetch: fetch } = utils;
+  let { region, apiKey } = config;
+  let plain_text = text.replaceAll("/", "@@");
+  let endpoint = "https://api.cognitive.microsofttranslator.com";
+  let url = `${endpoint}/translate?api-version=3.0&from=${from}&to=${to}`;
 
-    if (res.ok) {
-        let result = res.data;
-        const { translation } = result;
-        if (translation) {
-            return translation.replaceAll("@@", "/");;
-        } else {
-            throw JSON.stringify(result.trim());
-        }
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Ocp-Apim-Subscription-Key": apiKey,
+      "Ocp-Apim-Subscription-Region": region,
+      "Content-Type": "application/json",
+      "X-ClientTraceId": "c54424b2-b946-46f9-b726-46a60b3c9ce1",
+    },
+    body: { payload: [{ text: plain_text }], type: "Json" },
+  });
+
+  if (res.ok) {
+    let result = res.data;
+    const { translations } = result[0];
+    if (translations) {
+      const { text } = translations[0];
+      return text.replaceAll("@@", "/");
     } else {
-        throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
+      throw JSON.stringify(result);
     }
+  } else {
+    throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(
+      res.data,
+    )}`;
+  }
 }
